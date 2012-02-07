190ccaaa5b3ebf5ecbc84dd3cc6bcd22d714d019
Maxime Bourget <bmx007@gmail.com>
2012-02-07 15:00:55 +0000

    Insert trigger works but
    without any audit  information

--- bench_audit.rb
+++ bench_audit.rb
@@ -1,4 +1,4 @@
-$hide_sql = true
+$hide_sql = false
 def create_plate
 
   attributes = {}
@@ -26,15 +26,71 @@ def update_plate(plate)
 
 end
 
+def create_audit_table
+  con = Plate.connection
+  database = "#{con.current_database}_audit"
+  con.execute %Q{
+    CREATE DATABASE IF NOT EXISTS #{database};
+  }
+  con.execute %Q{
+    DROP TABLE IF EXISTS #{database}.assets;
+  }
+  con.execute %Q{
+    CREATE TABLE #{database}.assets
+-- (user VARCHAR(50), updated_at DATE, type VARCHAR(10))
+      SELECT assets.*
+      FROM assets
+      WHERE false;
+  }
+  #con.execute %Q{
+  #  CREATE VIEW assets_audit AS
+  #  SELECT * FROM #{database}.assets;
+  #}
+
+end
+def drop_audit_tables
+  con = Plate.connection
+  database = "#{con.current_database}_audit"
+  con.execute %Q{
+    DROP TABLE IF EXISTS #{database}.assets;
+  }
+  con.execute %Q{
+    DROP TRIGGER IF EXISTS assets_ari;
+  }
+end
+def create_audit_trigger
+  con = Plate.connection
+  database = "#{con.current_database}_audit"
+
+  con.execute %Q{
+    CREATE TRIGGER assets_ari
+    AFTER INSERT ON assets
+    FOR EACH ROW
+    BEGIN
+    INSERT into #{database}.assets
+      SELECT * FROM assets
+      WHERE id = NEW.id;
+    END;
+  }
 
 
+
+end
+
+def bench
+  Rails.logger.warn("******* Creating Plate *******")
+  p = create_plate
+  Rails.logger.warn("******* Updating Plate ******")
+  update_plate(p)
+end
 Rails.logger.warn("******* Normal sequence escape *****")
-Rails.logger.warn("******* Creating Plate *******")
-p = create_plate
-Rails.logger.warn("******* Updating Plate ******")
-update_plate(p)
+drop_audit_tables
+bench
 
+Rails.logger.warn("******* Create audit table ******")
+create_audit_table
+create_audit_trigger
 
-
+bench
 
 
